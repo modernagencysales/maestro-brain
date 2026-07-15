34ac3b82b37e6e89c91558a1bfe71cd73161860c
mini-headless <headless@modernagencysales.com>
2026-07-15 16:34:02 -0400

    feat: add stable Brain key resolvers

---
+++ packages/convex/confect/identity/stableKeys.spec.ts
@@ -0,0 +1,38 @@
+import { FunctionSpec, GroupSpec } from "@confect/core";
+import * as Schema from "effect/Schema";
+
+import { Id } from "../_generated/id";
+import { Unauthorized, ValidationFailed } from "../errors";
+import {
+  AgencyNotFound,
+  BrainNotFound,
+  StableKeyConflict,
+  TenantMismatch,
+} from "./stableKeys";
+
+export const ResolveBrainKeyArgs = Schema.Struct({
+  agencyKey: Schema.String,
+  brainKey: Schema.String,
+});
+
+export const ResolveBrainKeyReturns = Schema.Struct({
+  organizationId: Id("organizations"),
+  workspaceId: Id("workspaces"),
+});
+
+const resolveBrainKey = FunctionSpec.internalQuery({
+  name: "resolveBrainKey",
+  args: () => ResolveBrainKeyArgs,
+  returns: () => ResolveBrainKeyReturns,
+  error: () =>
+    Schema.Union(
+      Unauthorized,
+      ValidationFailed,
+      AgencyNotFound,
+      BrainNotFound,
+      StableKeyConflict,
+      TenantMismatch,
+    ),
+});
+
+export default GroupSpec.make().addFunction(resolveBrainKey);
