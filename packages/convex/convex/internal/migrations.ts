8667caf74d4320a3293b64a70135505e91201d7a
mini-headless <headless@modernagencysales.com>
2026-07-15 21:51:23 -0400

    fix: format generated migrations export

--- packages/convex/convex/internal/migrations.ts
+++ packages/convex/convex/internal/migrations.ts
@@ -1,10 +1,14 @@
 import registeredFunctions from "../../confect/_generated/registeredFunctions/internal/migrations";
 
 export const acquireLease = registeredFunctions.acquireLease;
-export const maybeCrashAfterComponent = registeredFunctions.maybeCrashAfterComponent;
+export const maybeCrashAfterComponent =
+  registeredFunctions.maybeCrashAfterComponent;
 export const probeExpand = registeredFunctions.probeExpand;
 export const probeFail = registeredFunctions.probeFail;
-export const runRegisteredMigration = registeredFunctions.runRegisteredMigration;
+export const runRegisteredMigration =
+  registeredFunctions.runRegisteredMigration;
 export const settleBatch = registeredFunctions.settleBatch;
-export const stableTenantOrganizationKeysExpand = registeredFunctions.stableTenantOrganizationKeysExpand;
-export const stableTenantWorkspaceKeysExpand = registeredFunctions.stableTenantWorkspaceKeysExpand;
+export const stableTenantOrganizationKeysExpand =
+  registeredFunctions.stableTenantOrganizationKeysExpand;
+export const stableTenantWorkspaceKeysExpand =
+  registeredFunctions.stableTenantWorkspaceKeysExpand;
