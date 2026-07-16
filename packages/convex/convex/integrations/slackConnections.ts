930098d9a45f5c053d850bc8ae4c4956855a73c4
mini-headless <headless@modernagencysales.com>
2026-07-16 18:14:38 -0400

    fix: format generated convex exports

--- packages/convex/convex/integrations/slackConnections.ts
+++ packages/convex/convex/integrations/slackConnections.ts
@@ -1,10 +1,16 @@
 import registeredFunctions from "../../confect/_generated/registeredFunctions/integrations/slackConnections";
 
-export const authorizeSlackConnectCompletion = registeredFunctions.authorizeSlackConnectCompletion;
+export const authorizeSlackConnectCompletion =
+  registeredFunctions.authorizeSlackConnectCompletion;
 export const beginSlackConnect = registeredFunctions.beginSlackConnect;
-export const claimSlackConnectAttempt = registeredFunctions.claimSlackConnectAttempt;
+export const claimSlackConnectAttempt =
+  registeredFunctions.claimSlackConnectAttempt;
 export const completeSlackConnect = registeredFunctions.completeSlackConnect;
-export const finalizeSlackConnectAttempt = registeredFunctions.finalizeSlackConnectAttempt;
-export const markSlackConnectAttemptFailed = registeredFunctions.markSlackConnectAttemptFailed;
-export const prepareSlackConnectAttempt = registeredFunctions.prepareSlackConnectAttempt;
-export const reconcileSlackConnectSessionExpiry = registeredFunctions.reconcileSlackConnectSessionExpiry;
+export const finalizeSlackConnectAttempt =
+  registeredFunctions.finalizeSlackConnectAttempt;
+export const markSlackConnectAttemptFailed =
+  registeredFunctions.markSlackConnectAttemptFailed;
+export const prepareSlackConnectAttempt =
+  registeredFunctions.prepareSlackConnectAttempt;
+export const reconcileSlackConnectSessionExpiry =
+  registeredFunctions.reconcileSlackConnectSessionExpiry;
