e12d2d6bc22a27c4e8e0bb69cb6938aab062f8c8
mini-headless <headless@modernagencysales.com>
2026-07-20 14:04:50 -0400

    feat: add source ledger contracts

---
+++ packages/convex/confect/tables/sourceRevisions.ts
@@ -0,0 +1,13 @@
+import { Table } from "@confect/server";
+
+import { SourceRevisionRow } from "../sources/sourceSchemas";
+
+export default Table.make(() => SourceRevisionRow)
+  .index("by_source_revision_key", ["organizationKey", "sourceRevisionKey"])
+  .index("by_source_provider_order", ["sourceKey", "providerOrder"])
+  .index("by_source_created", ["organizationKey", "sourceCreatedAt"])
+  .index("by_lifecycle_purge_after", [
+    "organizationKey",
+    "lifecycle.state",
+    "lifecycle.purgeAfter",
+  ]);
