10df346e1dfdcf43a81bb07caee17a87bd2ad645
mini-headless <headless@modernagencysales.com>
2026-07-20 17:20:46 -0400

    feat: define source ledger schemas

---
+++ packages/convex/confect/tables/sourceArtifacts.ts
@@ -0,0 +1,20 @@
+import { Table } from "@confect/server";
+
+import { SourceArtifactRow } from "../sources/sourceSchemas";
+
+export default Table.make(() => SourceArtifactRow)
+  .index("by_channel_provider_object", ["channelKey", "providerObjectId"])
+  .index("by_org_connection_generation_channel_provider_object", [
+    "organizationKey",
+    "connectionKey",
+    "connectionGeneration",
+    "channelKey",
+    "providerObjectId",
+  ])
+  .index("by_source_key", ["sourceKey"])
+  .index("by_thread_key", ["organizationKey", "threadKey"])
+  .index("by_lifecycle_purge_after", [
+    "organizationKey",
+    "lifecycle.state",
+    "lifecycle.purgeAfter",
+  ]);
