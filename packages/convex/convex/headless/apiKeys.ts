95b18a6afdbb99821f3614a3ed436619f9f878ad
mini-headless <headless@modernagencysales.com>
2026-07-20 04:00:35 -0400

    chore: refresh integration generated output

---
+++ packages/convex/convex/headless/apiKeys.ts
@@ -0,0 +1,8 @@
+import registeredFunctions from "../../confect/_generated/registeredFunctions/headless/apiKeys";
+
+export const authenticate = registeredFunctions.authenticate;
+export const create = registeredFunctions.create;
+export const list = registeredFunctions.list;
+export const markLastUsed = registeredFunctions.markLastUsed;
+export const revoke = registeredFunctions.revoke;
+export const rotate = registeredFunctions.rotate;
