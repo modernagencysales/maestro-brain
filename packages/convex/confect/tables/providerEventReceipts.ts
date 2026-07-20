7b9c1b80d673d81ed6822f2f9bff0972352549c2
mini-headless <headless@modernagencysales.com>
2026-07-20 08:27:55 -0400

    feat: add source ledger schema

---
+++ packages/convex/confect/tables/providerEventReceipts.ts
@@ -0,0 +1,12 @@
+import { Table } from "@confect/server";
+
+import { ProviderEventReceiptRow } from "../sources/sourceSchemas";
+
+export default Table.make(() => ProviderEventReceiptRow)
+  .index("by_connection_transport_delivery", [
+    "connectionKey",
+    "transportDeliveryId",
+  ])
+  .index("by_observation_key", ["observationKey"])
+  .index("by_received_at", ["organizationKey", "receivedAt"])
+  .index("by_outcome", ["organizationKey", "outcome"]);
