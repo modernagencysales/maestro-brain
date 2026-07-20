4c1ad35fd5f9b883920c9e5eaf5ea86c100e5f7d
mini-headless <headless@modernagencysales.com>
2026-07-20 08:22:39 -0400

    chore: refresh integration generated output

--- packages/convex/convex/brain/pages.ts
+++ packages/convex/convex/brain/pages.ts
@@ -6,5 +6,6 @@ export const favorite = registeredFunctions.favorite;
 export const get = registeredFunctions.get;
 export const list = registeredFunctions.list;
 export const move = registeredFunctions.move;
+export const recordSnapshot = registeredFunctions.recordSnapshot;
 export const recordSnapshotInternal = registeredFunctions.recordSnapshotInternal;
 export const rename = registeredFunctions.rename;
