1921a59d39032c2e59f0f5ec54c8ec6e15639817
mini-headless <headless@modernagencysales.com>
2026-08-03 18:30:41 -0400

    chore(convex): regenerate export capability refs

--- packages/convex/convex/ops/dataLifecycle.ts
+++ packages/convex/convex/ops/dataLifecycle.ts
@@ -1,4 +1,7 @@
 import registeredFunctions from "../../confect/_generated/registeredFunctions/ops/dataLifecycle";
 
 export const createDsarRequest = registeredFunctions.createDsarRequest;
+export const downloadBrainExport = registeredFunctions.downloadBrainExport;
+export const getBrainExport = registeredFunctions.getBrainExport;
 export const listDsarRequests = registeredFunctions.listDsarRequests;
+export const requestBrainExport = registeredFunctions.requestBrainExport;
